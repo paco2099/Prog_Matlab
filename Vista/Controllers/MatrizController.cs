84cc577c5bd7246e0c68cb7501d336b17fda5211
paco2099 <franciscogonzalez2099@gmail.com>
2023-03-09 20:27:43 -0600

    Agregar archivos de proyecto.

---
+++ Vista/Controllers/MatrizController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Vista.Controllers
+{
+    public class MatrizController : Controller
+    {
+        public IActionResult Tarea1(){
+            return View();
+        }
+        [HttpPost]
+        public JsonResult min(string matriz)
+        {
+            var filas = new List<String>(matriz.Split('\n'));
+
+            if (filas.Count == 1) {
+                var fila = new List<String>(filas[0].Split(' '));
+                var fila_int = fila.Select(int.Parse).ToList();
+                int valorMinimo = fila_int.Min();
+                return Json(valorMinimo.ToString());
+            }
+            else
+            {
+                var filasHD = new List<List<int>>();
+
+                for (int i = 0; i < filas.Count; i++)
+                {
+                    var fs = new List<String>(filas[i].Split(' '));
+                    var fs_int = fs.Select(int.Parse).ToList();
+                    filasHD.Add(fs_int);
+                }
+
+                var filasHDT = filasHD.SelectMany(inner => inner.Select((item, index) => new { item, index })).GroupBy(i => i.index, i => i.item).Select(g => g.ToList()).ToList();
+                var minCol = new List<int>();
+
+                for (int i = 0; i < filasHDT.Count; i++)
+                {
+                    minCol.Add(filasHDT[i].Min());
+                }
+
+                return Json(String.Join(" ", minCol));
+            }
+        }
+
+        
+    }
+}
