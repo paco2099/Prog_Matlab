80b53a46eac5dca1ea26165fdb197a1f72a1adeb
paco2099 <franciscogonzalez2099@gmail.com>
2023-03-16 21:47:40 -0600

    Para Actividad 3

---
+++ Vista/Controllers/Act3.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vista.Controllers
+{
+    public class Act3 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
